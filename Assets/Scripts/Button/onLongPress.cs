b3be1aa3efa28d9db8441aa0ed73d1fa06aef9c3
thành <tienthanh2171998@gmail.com>
2019-11-23 23:46:47 +0700

    Update Button UI

---
+++ Assets/Scripts/Button/onLongPress.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.Events;
+
+public class onLongPress : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    public UnityEvent onLongClick;
+    public UnityEvent onNotClick;
+    private bool isPressing;
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        isPressing = true;
+      
+        
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+       
+        isPressing = false;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isPressing)
+        {
+            onLongClick.Invoke();
+        } else
+        {
+            onNotClick.Invoke();
+        }
+    }
+}
---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public CharacterController2D controller;
+    [SerializeField] public int test;
+    float horizontalMove = 0f;
+    public float runSpeed ;
+    private Animator anim;
+    bool Jump = false;
+    bool isAtk = false;
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void jump()
+    {
+        
+    }
+
+    public void btn_leftOnClick()
+    {
+        anim.SetBool("isWalking", true);
+        if (this.anim.GetCurrentAnimatorStateInfo(0).IsTag("Attack")) return;
+        horizontalMove = -runSpeed;
+        if (Jump != true)
+            anim.SetBool("isWalking", true);
+        else anim.SetBool("isWalking", false);
+        controller.Move(horizontalMove, false, Jump);
+        
+    }
+
+    public void btn_rightOnClick()
+    {
+        
+        if (this.anim.GetCurrentAnimatorStateInfo(0).IsTag("Attack")) return;
+        horizontalMove = runSpeed;
+        if (Jump != true)
+            anim.SetBool("isWalking", true);
+        else anim.SetBool("isWalking", false);
+        controller.Move(horizontalMove, false, Jump);
+    }
+    public void onIddle()
+    {
+        
+        anim.SetBool("isWalking", false);
+    }
+}
