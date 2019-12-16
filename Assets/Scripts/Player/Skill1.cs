6e8a260ab06d7dbf461da0be0b9e85e008dbb010
Thành <tienthanh2171998@gmail.com>
2019-12-16 20:46:57 +0700

    comment code bị conflig

--- Assets/Scripts/Player/Skill1.cs
+++ Assets/Scripts/Player/Skill1.cs
@@ -24,50 +24,48 @@ public class Skill1 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(player.DirecCast == true)
-        {
-        RaycastHit2D hitInfo = Physics2D.Raycast(transform.position,transform.right * -1,distance,whatisSolid);
-           transform.localScale = new Vector3(-1,1,1);
-           transform.Translate(transform.right * speed * Time.deltaTime *-1);
-        if(hitInfo.collider != null)
-        {
-            try
-            {
-            hitInfo.collider.GetComponent<Enemy_move>().TakeDamage(damage);
-           //Debug.Log(hitInfo.rigidbody.ToString());
-            DestroySkill1();
-<<<<<<< Updated upstream
-            }
-            catch
-            {
-                Debug.Log("trung tuong");
-                DestroySkill1();
-            }
-=======
->>>>>>> Stashed changes
-        }
-        }
-        else
-        {
-        RaycastHit2D hitInfo = Physics2D.Raycast(transform.position,transform.right,distance);
-        transform.Translate(transform.right * speed * Time.deltaTime);
-        if(hitInfo.collider != null)
-        {
-            try
-            {
-            hitInfo.collider.GetComponent<Enemy_move>().TakeDamage(damage);
-            DestroySkill1();
-<<<<<<< Updated upstream
-            }
-            catch
-            {
-                Debug.Log("trung tuong");
-                DestroySkill1();
-            }
-=======
->>>>>>> Stashed changes
-        }
-        }
+//        if(player.DirecCast == true)
+//        {
+//        RaycastHit2D hitInfo = Physics2D.Raycast(transform.position,transform.right * -1,distance,whatisSolid);
+//           transform.localScale = new Vector3(-1,1,1);
+//           transform.Translate(transform.right * speed * Time.deltaTime *-1);
+//        if(hitInfo.collider != null)
+//        {
+//            try
+//            {
+//            hitInfo.collider.GetComponent<Enemy_move>().TakeDamage(damage);
+//           //Debug.Log(hitInfo.rigidbody.ToString());
+//            DestroySkill1();
+//<<<<<<< Updated upstream
+//            }
+//            catch
+//            {
+//                Debug.Log("trung tuong");
+//                DestroySkill1();
+//            }
+//=======
+//>>>>>>> Stashed changes
+//        }
+//        }
+//        else
+//        {
+//        RaycastHit2D hitInfo = Physics2D.Raycast(transform.position,transform.right,distance);
+//        transform.Translate(transform.right * speed * Time.deltaTime);
+//        if(hitInfo.collider != null)
+//        {
+//            try
+//            {
+//            hitInfo.collider.GetComponent<Enemy_move>().TakeDamage(damage);
+//            DestroySkill1();
+
+//            }
+//            catch
+//            {
+//                Debug.Log("trung tuong");
+//                DestroySkill1();
+//            }
+//        }
+//        }
     }
     void DestroySkill1()
     {
